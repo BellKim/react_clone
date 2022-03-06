52ce8b75823df5e7faaad3c1c8b969315d466f92
jonghyun <goste390@gmail.com>
2022-03-06 21:47:58 +0900

    useEffect() 함수 사용
    useEffect() 함수를 사용함으로서 전체 컴포넌트중에서 업데이트된 부분만 랜더링할수있도록 useEffect함수의 [] 배열에  useState 메소드 값을 넣어준다.

--- reactJS_movie/05_createREACTapp/react_start01/src/App.js
+++ reactJS_movie/05_createREACTapp/react_start01/src/App.js
@@ -1,13 +1,51 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import {useState, useEffect} from "react";
 
 
 function App() {
+
+    const[counter, setValue] = useState(0);
+    const[keyword, setKeyword] =useState("");
+
+    const onClick = () => setValue((prev) => prev +1);
+    console.log('i run all the time ');
+
+    const iRunonlyOnce = () => {
+        console.log("i run only one ");
+    }
+    const onChange = (event) => setKeyword(event.target.value);
+
+    //useEffect메소드를 사용해서 한번만 렌더링 되도록 할수 있다.
+    useEffect(iRunonlyOnce, []);
+    useEffect(() =>{
+        if(keyword !== "" && keyword.length > 6){
+            console.log("i run when 'keyworkd' changes : ", keyword);
+        }
+    }, [keyword]);
+
+    useEffect(() =>{
+        console.log("i run when 'counter' changes : ", counter);
+    }, [counter]);
+
+    useEffect(() =>{
+        console.log("i run when 'counter, keyword' changes : ", keyword, counter);
+    }, [keyword, counter]);
+
+    // [] 안에 useState 의 현재값이 변경되었을때만 코드를 실행하도록 react.js에게 알려준다.
+
+
   return (
     <div>
+        <input
+            type="text"
+            value={keyword}
+            placeholder="search here"
+            onChange={onChange}
+        />
 
-      <h1 className={styles.title}> first index react.</h1>
-        <Button text={"Continue"}/>
+        <h1 className={styles.title}> {counter}</h1>
+        <button onClick={onClick} > click me now </button>
 
     </div>
   );
