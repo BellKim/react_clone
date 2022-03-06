25ebe5aef3ec9ed9b25f8f6f5b947e2219d4fc9c
jonghyun <goste390@gmail.com>
2022-03-06 18:37:06 +0900

    style 추가방법

---
+++ reactJS_movie/05_createREACTapp/react_start01/src/App.js
@@ -0,0 +1,16 @@
+import Button from "./Button"
+import styles from "./App.module.css"
+
+
+function App() {
+  return (
+    <div>
+
+      <h1 className={styles.title}> first index react.</h1>
+        <Button text={"Continue"}/>
+
+    </div>
+  );
+}
+
+export default App;
