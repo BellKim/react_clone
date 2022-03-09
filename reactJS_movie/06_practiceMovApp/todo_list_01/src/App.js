8ba64181bcd49ce6e121a8ce8eeb8aece6ade495
jonghyun <goste390@gmail.com>
2022-03-09 16:57:16 +0900

    print todoList contents useing li tag.

--- reactJS_movie/06_practiceMovApp/todo_list_01/src/App.js
+++ reactJS_movie/06_practiceMovApp/todo_list_01/src/App.js
@@ -13,14 +13,13 @@ function App() {
         }
 
 
-        //항상 새로운 배열을  집어넣어야므로 복사된 배열을 넣어준다.
+        //항상 새로운 배열을  집어넣어야므로 복사된 배열을 넣어준다. (데이터를 추가해주는것)
+        //기존 배열을 가지고와서 새로운 요소를 가지고와 추가해준다.
         setTodos((currentArray) => [toDo, ...currentArray]);
-
-
-        console.log("todo value : " + toDo);
-        console.log("todos value : " + toDos);
-
+        //데이터를 업데이트 시켜줌.
         setTodo("");
+
+        console.log("todos value : " + toDos);
 
     }
 
@@ -36,7 +35,21 @@ function App() {
             />
             <button> add To Do </button>
         </form>
+        <hr/>
 
+        <ul>
+            {toDos.map((item, index) => (
+                <li key={index}>{item}</li>
+                )
+            )}
+        </ul>
+        {/*
+            var arr = [1,2,3,4,5];
+            모든 배열에 입력되는 값이 들어가게 한다.
+            arr.map(()=> ":)");
+            모든 배열의 값을 대문자로 변경되도록한다.
+            arr.map(()=>  toUpperCase());
+        */}
     </div>
   );
 }
