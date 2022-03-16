3e60567270f7dc7a8edc04f03a4662f0cc0031b4
jonghyun <goste390@gmail.com>
2022-03-16 11:44:03 +0900

    modify  data

--- reactJS_movie/06_practiceMovApp/chap03_movieApp/src/routes/Detail.js
+++ reactJS_movie/06_practiceMovApp/chap03_movieApp/src/routes/Detail.js
@@ -2,46 +2,39 @@ import React, {useEffect, useState} from 'react';
 import {Link}from "react-router-dom";
 import {useParams} from "react-router-dom";
 import Movie from "../components/Movie";
+// import Movie from "../components/Movie";
 
 function Detail() {
-    const {id} = useParams();
-    // console.log(id);
-
+    const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const { id } = useParams();
 
-    const getDetailMovie = async () => {
+    const getMovie = async () => {
         const json = await(
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
+
         console.log(json);
-        console.log(json.data.movie.title);
-         setMovies(json.data.movie);
-
         console.log(movies);
-
+        setMovies(json);
+        // setLoading(false);
+        console.log(movies);
     };
 
     useEffect(() => {
-        getDetailMovie();
+        getMovie(movies);
     }, []);
 
     return (
         <>
             <h1>Detail</h1>
-
             <a href="/">reload router:::: </a>
             <br/>
             <Link to="/">Link router ::::  </Link>
-            <hr />
-            {/*json.data.movie.title*/}
-            {/*<Movie*/}
-            {/*    key={movies.movie.id}*/}
-            {/*    id={movies.movie.id}*/}
-            {/*    coverImg={movies.movie.medium_cover_image}*/}
-            {/*    title={movies.movie.title}*/}
-            {/*    summary={movies.movie.summary}*/}
-            {/*    genres={movies.movie.genres}*/}
-            {/*/>*/}
+            <hr/>
+
+            <h1>{movies}</h1>
+
 
 
         </>
